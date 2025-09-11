6e4362544b160663c3851796b7df3cdba7248ea8
Anish <anish.bhupathiraju@outlook.com>
2025-09-11 14:22:10 +0530

    Fixed Yolo Issue

--- cv2-yolov12model.py
+++ cv2-yolov12model.py
@@ -1,1 +1,15 @@
-yolo detect predict model=yolov12l.pt source=0 show=true
+from ultralytics import YOLO
+
+
+model = YOLO("yolov12s.pt") #change the model using (n,s,m,l,x)
+
+for result in model.predict(source=0, show=True, imgsz=640, device=0):
+    print(result.boxes)  
+
+
+
+#Bash : Run in cmd:
+# After creating a virtual environment and installing ultralytics using pip install ultralytics
+
+
+#yolo detect predict model=yolo12l.pt source=0 show=true
